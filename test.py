f7759144b6ffccce0205fb722d84ee94696dc710
jairNeto <jguedesf@uci.edu>
2018-02-08 09:55:32 -0300

    First Commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import cv2
+from matplotlib import pyplot as plt
+
+car_cascade = cv2.CascadeClassifier('cars.xml')
+img = cv2.imread('car5.jpg', 1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect cars
+cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+# Draw border
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
+
+# Show image
+plt.figure(figsize=(10,20))
+plt.imshow(img)
+plt.show()
